50cb96861059ff11681d1f9bcf42cbb01bd2f17d
Andrey Karamnoff <karamnoff@gmail.com>
2019-12-12 22:50:08 +0300

    add solution for 03-dispatch-data-by-key

--- html.js
+++ html.js
@@ -1,5 +1,22 @@
+/* eslint-disable no-unused-vars */
 import _ from 'lodash';
 
 // BEGIN (write your solution here)
 
+const acceptableTagsList = new Set(['a', 'link', 'img']);
+const acceptableTagsAttrs = {
+  a: 'href',
+  link: 'href',
+  img: 'src',
+};
+
+export default tags => {
+  const filtered = tags.filter(({ name }) => acceptableTagsList.has(name));
+  const mapped = filtered.map(value => {
+    const { name } = value;
+    const attr = acceptableTagsAttrs[name];
+    return value[attr];
+  });
+  return mapped;
+};
 // END
