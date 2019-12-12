e7192655ead26843b092eb3ecddeb358be25d878
Andrey Karamnoff <karamnoff@gmail.com>
2019-12-12 22:13:58 +0300

    add solution for 02-parametric-polymorphism

--- linkedList.js
+++ linkedList.js
@@ -1,5 +1,14 @@
 import Node from './Node';
 
-// BEGIN (write your solution here)
+const reverse = (list) => {
+  const iter = (acc, items) => {
+    if (items === null) {
+      return acc;
+    }
+    const newAcc = new Node(items.getValue(), acc);
+    return iter(newAcc, items.getNext());
+  };
+  return iter(null, list);
+};
 
-// END
+export default reverse;
