edf405161010b1cb184c1515785a28169ea9309a
Andrey Karamnoff <karamnoff@gmail.com>
2019-12-13 08:18:25 +0300

    add solution for 04-dispatch-functions-by-key

--- html.js
+++ html.js
@@ -1,3 +1,23 @@
 // BEGIN (write your solution here)
+const createAttrs = ({ class: className = '', id = '', randomAttr = '' }) => {
+  const classAttr = className !== '' ? ` class="${className}"` : '';
+  const idAttr = id !== '' ? ` id="${id}"` : '';
+  const rand = randomAttr !== '' ? ` randomAttr="${randomAttr}"` : '';
+  return `${classAttr}${idAttr}${rand}`;
+};
+const mapping = {
+  pair: ({ name, body, ...rest }) => {
+    const attrs = createAttrs(rest);
+    return `<${name}${attrs}>${body}</${name}>`;
+  },
+  single: ({ name, ...rest }) => {
+    const attrs = createAttrs(rest);
+    return `<${name}${attrs}>`;
+  },
+};
 
+export default tag => {
+  const { tagType } = tag;
+  return mapping[tagType](tag);
+};
 // END
