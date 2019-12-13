f7f24d12093a5b18f7a08c73b8a4e5c42d9dda5d
Andrey Karamnoff <karamnoff@gmail.com>
2019-12-13 23:42:10 +0300

    add solution for 05-dispatch-by-file

--- DatabaseConfigLoader.js
+++ DatabaseConfigLoader.js
@@ -4,6 +4,28 @@ import _ from 'lodash';
 
 // BEGIN (write your solution here)
 export default class {
-  //
+  constructor(pathToConfigs) {
+    this.pathToConfigs = pathToConfigs;
+  }
+
+  load(env) {
+    const config = JSON.parse(
+      fs.readFileSync(
+        path.join(this.pathToConfigs, `database.${env}.json`),
+        'utf8',
+      ),
+    );
+    if (Object.keys(config).includes('extend')) {
+      const { extend, ...rest } = config;
+      const extendedConfig = JSON.parse(
+        fs.readFileSync(
+          path.join(this.pathToConfigs, `database.${extend}.json`),
+          'utf8',
+        ),
+      );
+      return { ...extendedConfig, ...rest };
+    }
+    return config;
+  }
 }
 // END
