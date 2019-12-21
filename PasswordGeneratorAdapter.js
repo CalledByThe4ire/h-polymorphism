714c78d242f76fb3a7e02dcb82a9c297da4cdcbf
Andrey Karamnoff <karamnoff@gmail.com>
2019-12-21 20:32:12 +0300

    add hexlet' solution for 13-composition

--- PasswordGeneratorAdapter.js
+++ PasswordGeneratorAdapter.js
@@ -1,28 +1,22 @@
 import generator from 'generate-password';
 
-const defaultConfig = {
-  numbers: false,
-  symbols: false,
-  uppercase: false,
-};
-
 export default class PasswordGeneratorAdapter {
   // BEGIN (write your solution here)
   constructor() {
-    this.generator = generator;
+    this.options = {
+      symbols: false,
+      numbers: false,
+      uppercase: false,
+    };
   }
 
   generatePassword(length, options) {
-    const config = options.length === 0
-      ? defaultConfig
-      : options.reduce(
-        (acc, option) => ({
-          ...acc,
-          [option]: defaultConfig[option],
-        }),
-        { length },
-      );
-    return this.generator.generate(config);
+    options.forEach(option => {
+      this.options[option] = true;
+    });
+    this.options.length = length;
+
+    return generator.generate(this.options);
   }
   // END
 }
