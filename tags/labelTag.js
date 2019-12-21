9d97b6761be097c80aa433271cb006d09b5ebd41
Andrey Karamnoff <karamnoff@gmail.com>
2019-12-21 20:35:46 +0300

    add solution for 12-patterns

--- tags/labelTag.js
+++ tags/labelTag.js
@@ -1,3 +1,14 @@
 export default class LabelTag {
-  //
+  constructor(value, tag) {
+    this.value = value;
+    this.tag = tag;
+  }
+
+  render() {
+    return `<label>${this.value}${this.tag}</label>`;
+  }
+
+  toString() {
+    return this.render();
+  }
 }
